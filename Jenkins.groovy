70340b82d88e21a0acb094c7508abbcd41193c05
Q-Chakorn <chakornotaku@gmail.com>
2025-08-11 17:05:15 +0700

    Update Jenkins.groovy

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -8,16 +8,15 @@
 //         }
 //     }
 // }
-{
-    node(macbook){
-        checkout scm
-        cleanWs()
-        stage('gitclone'){
-            withCredentials([secretText(credentialsId: 'github-connec', variable: 'GIT_CREDENTIALS')]) {
-                sh """
-                    git clone https://github.com/Q-Chakorn/testiot.git
-                """
-            }
+
+node(macbook){
+    checkout scm
+    cleanWs()
+    stage('gitclone'){
+        withCredentials([secretText(credentialsId: 'github-connec', variable: 'GIT_CREDENTIALS')]) {
+            sh """
+                git clone https://github.com/Q-Chakorn/testiot.git
+            """
         }
     }
 }
