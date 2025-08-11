272bf6475e135ec4b01ec7c9fcac88866447be46
Q-Chakorn <chakornotaku@gmail.com>
2025-08-11 17:54:29 +0700

    Update Jenkins.groovy

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -8,13 +8,32 @@
 //         }
 //     }
 // }
-
+properties([
+    parameters([
+        choice(name: 'SERVICES', choices: ['datalogger-agent', 'iaq-agent'], description: '')
+    ])
+])
+def datalogPath = "/Users/ikkyu/root/workspace/test/testiot/testiot/datalogger-agent"
+def iaqPath = "/Users/ikkyu/root/workspace/test/testiot/testiot/iaq-agent"
 node("macbook"){
     checkout scm
     cleanWs()
     stage('gitclone'){
         sh """
             git clone https://github.com/Q-Chakorn/testiot.git
+            cd ${datalogPath} && ls -la
+            cd ${iaqPath} && ls -la
         """
+    }
+    stage('build images'){
+        if (params.SERVICES == 'datalogger-agent') {
+            sh """
+                cd ${datalogPath} && ls -la
+            """
+        } else if (params.SERVICES == 'iaq-agent') {
+            sh """
+                cd ${iaqPath} && ls -la
+            """
+        }
     }
 }
