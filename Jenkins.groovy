3a51e5da3223c53ddb3d7ec268572a15e0c14e85
Q-Chakorn <chakornotaku@gmail.com>
2025-08-11 17:04:01 +0700

    Update Jenkins.groovy

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -1,9 +1,22 @@
-pipeline {
-    agent any
-    stages {
-        stage('Checkout') {
-            steps {
-               checkout scm
+// pipeline {
+//     agent any
+//     stages {
+//         stage('Checkout') {
+//             steps {
+//                checkout scm
+//             }
+//         }
+//     }
+// }
+{
+    node(macbook){
+        checkout scm
+        cleanWs()
+        stage('gitclone'){
+            withCredentials([secretText(credentialsId: 'github-connec', variable: 'GIT_CREDENTIALS')]) {
+                sh """
+                    git clone https://github.com/Q-Chakorn/testiot.git
+                """
             }
         }
     }
