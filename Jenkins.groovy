0c38621c7dfe5bd994d130dfd148c7ab7c178f10
Q-Chakorn <chakornotaku@gmail.com>
2025-08-11 23:10:38 +0700

    Update Jenkins.groovy

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -8,14 +8,15 @@
 //         }
 //     }
 // }
+def datalogPath = "/Users/ikkyu/root/workspace/test/testiot/testiot/datalogger-agent"
+def iaqPath = "/Users/ikkyu/root/workspace/test/testiot/testiot/iaq-agent"
+def deployPath = "/home/testdevops/testiot"
 properties([
     parameters([
         choice(name: 'SERVICES', choices: ['datalogger-agent', 'iaq-agent'], description: ''),
         string(name: 'TAG', defaultValue: '', description: 'Docker image tag')
     ])
 ])
-def datalogPath = "/Users/ikkyu/root/workspace/test/testiot/testiot/datalogger-agent"
-def iaqPath = "/Users/ikkyu/root/workspace/test/testiot/testiot/iaq-agent"
 node("macbook"){
     checkout scm
     cleanWs()
@@ -30,14 +31,14 @@ node("macbook"){
         if (params.SERVICES == 'datalogger-agent') {
             sh """
                 cd ${datalogPath} && ls -la
-                docker build -t datalogger-agent:${TAG} .
-                docker tag datalogger-agent:${TAG} testiotacr/datalogger-agent:${TAG}
+                docker buildx build --platform linux/amd64 -t datalogger-agent:${TAG} .
+                docker tag datalogger-agent:${TAG} testiotacr.azurecr.io/datalogger-agent:${TAG}
             """
         } else if (params.SERVICES == 'iaq-agent') {
             sh """
                 cd ${iaqPath} && ls -la
-                docker build -t iaq-agent:${TAG} .
-                docker tag iaq-agent:${TAG} testiotacr/iaq-agent:${TAG}
+                docker buildx build --platform linux/amd64 -t iaq-agent:${TAG} .
+                docker tag iaq-agent:${TAG} testiotacr.azurecr.io/iaq-agent:${TAG}
             """
         }
     }
@@ -45,33 +46,70 @@ node("macbook"){
         withCredentials([azureServicePrincipal('azure-credentials')]) {
             if (params.SERVICES == 'datalogger-agent') {
                 sh """
+                    az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
                     az acr login --name testiotacr
-                    docker push testiotacr/datalogger-agent:${TAG}
+                    docker push testiotacr.azurecr.io/datalogger-agent:${TAG}
                 """
             } else if (params.SERVICES == 'iaq-agent') {
                 sh """
+                    az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
                     az acr login --name testiotacr
-                    docker push testiotacr/iaq-agent:${TAG}
+                    docker push testiotacr.azurecr.io/iaq-agent:${TAG}
                 """
             }
         }
     }
-    // stage('deploy'){
-    //     sshagent(['ssh-credentials']) {
-    //         sh"""
-    //             ssh -o StrictHostKeyChecking=no testdevops@20.6.33.223 "echo 'Connected to remote server'"
-    //             ssh -o StrictHostKeyChecking=no testdevops@20.6.33.223 "
-    //                 if [ -d 'testiot' ]; then
-    //                     echo 'Directory testiot exists, pulling latest changes'
-    //                     cd testiot && git pull
-    //                 else
-    //                     echo 'Directory testiot does not exist, cloning repository'
-    //                     git clone https://github.com/Q-Chakorn/testiot.git
-    //                 fi
-    //             pwd
-    //             ls -la
-    //             "
-    //         """
-    //     }
-    // }
+    stage('pull image from acr'){
+        sshagent(['ssh-credentials']) {
+            withCredentials([azureServicePrincipal('azure-credentials')]){
+                if (params.SERVICES == 'datalogger-agent') {
+                    sh"""
+                        ssh -o StrictHostKeyChecking=no testdevops@20.6.33.223 '''
+                        az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
+                        az acr login --name testiotacr
+                        docker pull testiotacr.azurecr.io/datalogger-agent:${TAG}
+                        '''
+                    """
+                }else if (params.SERVICES == 'iaq-agent') {
+                    sh"""
+                        ssh -o StrictHostKeyChecking=no testdevops@20.6.33.223 '''
+                        az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
+                        az acr login --name testiotacr
+                        docker pull testiotacr.azurecr.io/iaq-agent:${TAG}
+                        '''
+                    """                
+                }
+            }
+        }
+    }
+    stage('prepare'){
+        sshagent(['ssh-credentials']) {
+            sh"""
+                ssh -o StrictHostKeyChecking=no testdevops@20.6.33.223 '''
+                    if [ -d 'testiot' ]; then
+                        echo 'Directory testiot exists, pulling latest changes'
+                        cd testiot && git pull
+                    else
+                        echo 'Directory testiot does not exist, cloning repository'
+                        git clone https://github.com/Q-Chakorn/testiot.git
+                    fi
+                cd ${deployPath}
+                docker-compose -f 4.datalogger-agent.yaml down
+                '''
+            """
+        }
+    }
+    stage('deploy'){
+        sshagent(['ssh-credentials']) {
+            if (params.SERVICES == 'datalogger-agent') {
+                sh """
+                    ssh -o StrictHostKeyChecking=no testdevops@20.6.33.223 '''
+                    cd ${deployPath}
+                    ls -la
+                    docker-compose -f 4.datalogger-agent.yaml up -d
+                    '''
+                """
+            }
+        }
+    }
 }
